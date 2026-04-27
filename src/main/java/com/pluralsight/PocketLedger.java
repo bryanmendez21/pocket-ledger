b49eddf583413f21daacbf24ebee145728a41dad
bryan <bryanmendez21@icloud.com>
2026-04-27 12:16:32 -0400

    - Removed Prompt asking if i wanted to return so it can automatically return when h is pressed - moved ledger menu into its own method

--- src/main/java/com/pluralsight/PocketLedger.java
+++ src/main/java/com/pluralsight/PocketLedger.java
@@ -4,11 +4,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class PocketLedger {
+    static  Scanner userInput = new Scanner(System.in); // shared across entire class
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
 
         boolean running = true;
-        String returnToMain;
 
         do {
             System.out.println("---------- Welcome to Pocket Ledger ----------");
@@ -16,10 +15,10 @@ public class PocketLedger {
             System.out.println("P) Make Payment (Debit)");
             System.out.println("L) Ledger");
             System.out.println("X) Exit");
-            System.out.println("Choose Option Using Letters: ");
-            String pocketLedgerMenu = userInput.nextLine();
+            System.out.print("Choose Option Using Letters: ");
+            String homeChoice = userInput.nextLine();
 
-            switch (pocketLedgerMenu){
+            switch (homeChoice){
                 case "D", "d" -> {
                     System.out.println("Deposit");
                     break;
@@ -29,33 +28,8 @@ public class PocketLedger {
                     break;
                 }
                 case "L", "l" -> {
-                    System.out.println("---------- Ledger Menu ----------");
-                    System.out.println("A) Display All");
-                    System.out.println("D) Deposits");
-                    System.out.println("P) Payments");
-                    System.out.println("R) Reports");
-                    System.out.println("Choose Option Using Letters: ");
-                    String ledgerMenuInput = userInput.nextLine();
-                    switch (ledgerMenuInput){
-                        case "A","a" ->{
-                            System.out.println("Display all");
-                            break;
-                        }
-                        case "D","d" ->{
-                            System.out.println("view deposits");
-                            break;
-                        }
-                        case "P","p" ->{
-                            System.out.println(" view payments");
-                            break;
-                        }
-                        case "R","r" ->{
-                            System.out.println("reports");
-                            break;
-                        }
-                    }
-
-                    break;
+                   ledgerMenu();
+                   break;
                 }
                 case "X", "x" ->{
                     running = false;
@@ -68,16 +42,48 @@ public class PocketLedger {
             }
 
 
-
-            System.out.print("Return to main menu: (yes/no): ");
-            returnToMain = userInput.nextLine();
-
-            if (returnToMain.equalsIgnoreCase("no")) {
-                running = false;
-            }
         } while (running);
             userInput.close();
             System.out.println("Good Bye!");
 
     }
+
+    // Menu Methods
+    public static void ledgerMenu(){
+        System.out.println("---------- Ledger Menu ----------");
+        System.out.println("A) Display All");
+        System.out.println("D) Deposits");
+        System.out.println("P) Payments");
+        System.out.println("R) Reports");
+        System.out.println("H) Home");
+        System.out.print("Choose Option Using Letters: ");
+        String ledgerChoice = userInput.nextLine();
+        switch (ledgerChoice){
+            case "A","a" ->{
+                System.out.println("Display all");
+                break;
+            }
+            case "D","d" ->{
+                System.out.println("view deposits");
+                break;
+            }
+            case "P","p" ->{
+                System.out.println(" view payments");
+                break;
+            }
+            case "R","r" ->{
+                System.out.println("reports");
+                break;
+            }
+            case "H","h" ->{
+                return; // exits method and returns main menu
+
+            }
+            default -> {
+                System.out.println("Invalid Input");
+                break;
+            }
+        }
+    }
+
 }
