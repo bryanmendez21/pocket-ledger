f8692e4051794e06184f4f21305049edc614612d
bryan <bryanmendez21@icloud.com>
2026-04-27 11:10:25 -0400

    - Add main menu with D, P, L, x options - Implemented do-while loop to keep app running - Add exit prompt and goodbye message

--- src/main/java/com/pluralsight/PocketLedger.java
+++ src/main/java/com/pluralsight/PocketLedger.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PocketLedger {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        boolean running = true;
+        String returnToMain;
+
+        do {
+            System.out.println("---------- Welcome to Pocket Ledger ----------");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+
+            System.out.print("Would you want to go back (yes/no): ");
+            returnToMain = userInput.nextLine();
+
+            if (returnToMain.equalsIgnoreCase("no")) {
+                running = false;
+            }
+        } while (running);
+            userInput.close();
+            System.out.println("Good Bye!");
+
+    }
 }
