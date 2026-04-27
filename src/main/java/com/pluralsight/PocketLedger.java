0231293e29414a60e0fc3bb56d458fc7fce6718b
bryan <bryanmendez21@icloud.com>
2026-04-27 11:40:45 -0400

    - Add ledger menu - Updated main menu so lower and upper case can be used

--- src/main/java/com/pluralsight/PocketLedger.java
+++ src/main/java/com/pluralsight/PocketLedger.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class PocketLedger {
@@ -15,8 +16,60 @@ public class PocketLedger {
             System.out.println("P) Make Payment (Debit)");
             System.out.println("L) Ledger");
             System.out.println("X) Exit");
+            System.out.println("Choose Option Using Letters: ");
+            String pocketLedgerMenu = userInput.nextLine();
 
-            System.out.print("Would you want to go back (yes/no): ");
+            switch (pocketLedgerMenu){
+                case "D", "d" -> {
+                    System.out.println("Deposit");
+                    break;
+                }
+                case "P", "p" -> {
+                    System.out.println("Payment");
+                    break;
+                }
+                case "L", "l" -> {
+                    System.out.println("---------- Ledger Menu ----------");
+                    System.out.println("A) Display All");
+                    System.out.println("D) Deposits");
+                    System.out.println("P) Payments");
+                    System.out.println("R) Reports");
+                    System.out.println("Choose Option Using Letters: ");
+                    String ledgerMenuInput = userInput.nextLine();
+                    switch (ledgerMenuInput){
+                        case "A","a" ->{
+                            System.out.println("Display all");
+                            break;
+                        }
+                        case "D","d" ->{
+                            System.out.println("view deposits");
+                            break;
+                        }
+                        case "P","p" ->{
+                            System.out.println(" view payments");
+                            break;
+                        }
+                        case "R","r" ->{
+                            System.out.println("reports");
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+                case "X", "x" ->{
+                    running = false;
+                    continue;
+                }
+                default -> {
+                    System.out.println("Invalid Input");
+                    break;
+                }
+            }
+
+
+
+            System.out.print("Return to main menu: (yes/no): ");
             returnToMain = userInput.nextLine();
 
             if (returnToMain.equalsIgnoreCase("no")) {
