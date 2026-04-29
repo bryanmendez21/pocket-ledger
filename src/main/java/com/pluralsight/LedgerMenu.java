aeb55be611313f83e879bbd8a222cedb59cc80fc
bryan <bryanmendez21@icloud.com>
2026-04-29 12:57:04 -0400

    - added do while loop to report menu and ledger menu so user can stay in that screen unless they decide to go back

--- src/main/java/com/pluralsight/LedgerMenu.java
+++ src/main/java/com/pluralsight/LedgerMenu.java
@@ -7,64 +7,67 @@ import java.util.ArrayList;
 
 public class LedgerMenu {
     public static void ledgerMenu() {
-        try(BufferedReader tranFile = FileManager.getReader()) { // auto closes Reader
-            System.out.println("---------- Ledger Menu ----------");
-            System.out.println("A) Display All");
-            System.out.println("D) Deposits");
-            System.out.println("P) Payments");
-            System.out.println("R) Reports");
-            System.out.println("H) Home");
-            System.out.print("Choose Option Using Letters: ");
-            String ledgerChoice = PocketLedger.userInput.nextLine();
+        boolean ledgerRunning = true;
+        do {
+            try (BufferedReader tranFile = FileManager.getReader()) { // auto closes Reader
+                System.out.println("---------- Ledger Menu ----------");
+                System.out.println("A) Display All");
+                System.out.println("D) Deposits");
+                System.out.println("P) Payments");
+                System.out.println("R) Reports");
+                System.out.println("H) Home");
+                System.out.print("Choose Option Using Letters: ");
+                String ledgerChoice = PocketLedger.userInput.nextLine();
 
-            ArrayList<Transactions> transactions = Transactions.loadTransaction(); // get whole list
-            switch (ledgerChoice) {
-                case "A", "a" -> {
-                    LedgerChoice("all");
-                   // System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
-                   // for (Transactions t : transactions) { // iterate through list
-                    //    System.out.printf("%s | %s | %s | %s | %.2f\n", t.getDate(),t.getTime(),t.getVendor(),t.getDescription(),t.getAmount());
-                   // }
-                    break;
-                }
-                case "D", "d" -> { // move a,d,p into one method and call based on whats needed
-                    System.out.println("---------- DEPOSITS ----------");
-                    LedgerChoice("deposits");
-                   // System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
-                   // for (Transactions t : transactions) { // iterate through list
-                       // if(t.getAmount() > 0){
-                      //      System.out.printf("%s | %s | %s | %s | %.2f\n", t.getDate(),t.getTime(),t.getVendor(),t.getDescription(),t.getAmount());
-                     //   }
-                   // }
-                    break;
-                }
-                case "P", "p" -> {
-                    System.out.println("---------- PAYMENTS ----------");
-                    LedgerChoice("payments");
-                   // System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
-                   // for (Transactions t : transactions) { // iterate through list
+                ArrayList<Transactions> transactions = Transactions.loadTransaction(); // get whole list
+                switch (ledgerChoice) {
+                    case "A", "a" -> {
+                        LedgerChoice("all");
+                        // System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
+                        // for (Transactions t : transactions) { // iterate through list
+                        //    System.out.printf("%s | %s | %s | %s | %.2f\n", t.getDate(),t.getTime(),t.getVendor(),t.getDescription(),t.getAmount());
+                        // }
+                        break;
+                    }
+                    case "D", "d" -> { // move a,d,p into one method and call based on whats needed
+                        System.out.println("---------- DEPOSITS ----------");
+                        LedgerChoice("deposits");
+                        // System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
+                        // for (Transactions t : transactions) { // iterate through list
+                        // if(t.getAmount() > 0){
+                        //      System.out.printf("%s | %s | %s | %s | %.2f\n", t.getDate(),t.getTime(),t.getVendor(),t.getDescription(),t.getAmount());
+                        //   }
+                        // }
+                        break;
+                    }
+                    case "P", "p" -> {
+                        System.out.println("---------- PAYMENTS ----------");
+                        LedgerChoice("payments");
+                        // System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
+                        // for (Transactions t : transactions) { // iterate through list
                         //if(t.getAmount() < 0){
                         //    System.out.printf("%s | %s | %s | %s | %.2f\n", t.getDate(),t.getTime(),t.getVendor(),t.getDescription(),t.getAmount());
-                       // }
-                   // }
-                    break;
-                }
-                case "R", "r" -> {
-                    ReportMenu.reportMenu();
-                    break;
-                }
-                case "H", "h" -> {
-                    return; // exits method and returns main menu
+                        // }
+                        // }
+                        break;
+                    }
+                    case "R", "r" -> {
+                        ReportMenu.reportMenu();
+                        break;
+                    }
+                    case "H", "h" -> {
+                        ledgerRunning = false;
 
+                    }
+                    default -> {
+                        System.out.println("Invalid Input");
+                        break;
+                    }
                 }
-                default -> {
-                    System.out.println("Invalid Input");
-                    break;
-                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        }while(ledgerRunning);
     }
     public static void LedgerChoice(String filter){ //filter by string
         System.out.println("    DATE   |   TIME   |  VENDOR  |   DESCRIPTION  |  AMOUNT");
