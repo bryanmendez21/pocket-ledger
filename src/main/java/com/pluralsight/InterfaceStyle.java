dc78bb73483db5614c648eda6ad86fdd3cdd2678
bryan <bryanmendez21@icloud.com>
2026-04-30 12:47:10 -0400

    - implement more ansi colors and characters for border and better design - applied ansi to my print pocketledger menu.    - moved initial string into a variable in order to get the right border aligned properly since string length was not the same for each one

--- src/main/java/com/pluralsight/InterfaceStyle.java
+++ src/main/java/com/pluralsight/InterfaceStyle.java
@@ -1,33 +1,47 @@
 package com.pluralsight;
 
 public class InterfaceStyle {
-    public static void main(String[] args) {
-        // --- ANSI COLORS ---
-        String RESET   = "\u001B[0m";
-        String BLACK   = "\u001B[30m";
-        String CYAN    = "\u001B[36m";
-        String RED     = "\u001B[31m";
-        String GREEN   = "\u001B[32m";
-        String MAGENTA = "\u001B[35m";
-        String BOLD    = "\u001B[1m";
 
-        // --- BORDER CHARACTERS ---
-        final String TL = "╔";  // top-left
-        final String TR = "╗";  // top-right
-        final String BL = "╚";  // bottom-left
-        final String BR = "╝";  // bottom-right
-        final String H  = "═";  // horizontal
-        final String V  = "║";  // vertical
+    // --- ANSI COLORS ---
+    public static final String RESET   = "\u001B[0m"; // reset so colors won't bleed into next line
+    public static final String WHITE   = "\u001B[37m";
+    public static final String BLACK   = "\u001B[30m";
+    public static final String CYAN    = "\u001B[36m";
+    public static final String RED     = "\u001B[31m";
+    public static final String GREEN   = "\u001B[32m";
+    public static final String MAGENTA = "\u001B[35m";
+    public static final String YELLOW  = "\u001B[33m";
+    public static final String BLUE    = "\u001B[34m";
+    public static final String BOLD    = "\u001B[1m";
 
-        // --- BORDER WIDTH ---
-        int WIDTH = 50;
+    // --- ANSI BRIGHT COLORS ---
+    public static final String BG_BRIGHT_BLACK   = "\u001B[100m";
+    public static final String BG_BRIGHT_RED     = "\u001B[101m";
+    public static final String BG_BRIGHT_GREEN   = "\u001B[102m";
+    public static final String BG_BRIGHT_YELLOW  = "\u001B[103m";
+    public static final String BG_BRIGHT_BLUE    = "\u001B[104m";
+    public static final String BG_BRIGHT_MAGENTA = "\u001B[105m";
+    public static final String BG_BRIGHT_CYAN    = "\u001B[106m";
+    public static final String BG_BRIGHT_WHITE   = "\u001B[107m";
 
-        // --- TEST ---
-        System.out.println(BLACK + BOLD + TL + H.repeat(WIDTH) + TR + RESET);
-        System.out.println(BLACK + BOLD + V  + RESET + CYAN + "  » View Balance" + RESET);
-        System.out.println(BLACK + BOLD + V  + RESET + GREEN + "  + Deposit $500" + RESET);
-        System.out.println(BLACK + BOLD + V  + RESET + RED + "  - Payment $200" + RESET);
-        System.out.println(BLACK + BOLD + V  + RESET + MAGENTA + "  ← Go Back" + RESET);
-        System.out.println(BLACK + BOLD + BL + H.repeat(WIDTH) + BR + RESET);
-    }
+    // --- BORDER CHARACTERS ---
+    public static final String TL = WHITE + BOLD + "╔"+ RESET;
+    public static final String TR = WHITE + BOLD + "╗"+ RESET;
+    public static final String BL = WHITE + BOLD + "╚"+ RESET;
+    public static final String BR = WHITE + BOLD + "╝"+ RESET;
+    public static final String ML = "╠"; // middle-left
+    public static final String MR = "╣"; // middle-right
+    public static final String H  = "═"; // RULE: Don't put colors on repeated thing
+    public static final String WALL = WHITE + BOLD + "║" + RESET;
+    // --- BORDER WIDTH ---
+    public static final int WIDTH = 40;
+
+    // --- TEST --- add main to test again
+    //System.out.println(WHITE + BOLD + TL + H.repeat(WIDTH) + TR + RESET);
+    //System.out.println(WHITE + BOLD + V  + RESET + CYAN + "  » View Balance" + RESET);
+    //System.out.println(WHITE + BOLD + V  + RESET + GREEN + "  + Deposit $500" + RESET);
+    //System.out.println(WHITE + BOLD + V  + RESET + RED + "  - Payment $200" + RESET);
+    //System.out.println(WHITE + BOLD + V  + RESET + MAGENTA + "  ← Go Back" + RESET);
+    //System.out.println(WHITE + BOLD + BL + H.repeat(WIDTH) + BR + RESET);
+
 }
--- src/main/java/com/pluralsight/PocketLedger.java
+++ src/main/java/com/pluralsight/PocketLedger.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.io.*;
 import java.util.Scanner;
+import static com.pluralsight.InterfaceStyle.*;
 
 public class PocketLedger {
     static  Scanner userInput = new Scanner(System.in); // shared across entire class
@@ -10,13 +11,24 @@ public class PocketLedger {
         boolean running = true;
 
         do {
-            System.out.println("---------- Welcome to Pocket Ledger ----------");
-            System.out.println("D) Add Deposit");
-            System.out.println("P) Make Payment");
-            System.out.println("L) Ledger");
-            System.out.println("X) Exit");
-            System.out.print("Choose Option Using Letters: ");
+            System.out.println(TL + WHITE + BOLD + H.repeat(WIDTH) + TR + RESET);// Top Border
+            String t = "        Welcome to Pocket Ledger ";
+            System.out.println(WALL + BOLD + t + " ".repeat(WIDTH - t.length()) + WALL);
+            System.out.println(WHITE + BOLD + ML + H.repeat(WIDTH) + MR + RESET); // Divider
+            String d = "D) Add Deposit";
+            System.out.println(WALL + CYAN + BOLD +  d + " ".repeat(WIDTH - d.length()) + RESET + WALL);
+            String p = "P) Make Payment";
+            System.out.println(WALL + CYAN + BOLD + p + " ".repeat(WIDTH - p.length()) + RESET + WALL);
+            String l = "L) Ledger";
+            System.out.println(WALL + CYAN + BOLD + l + " ".repeat(WIDTH - l.length()) + RESET + WALL);
+            String x = "X) Exit";
+            System.out.println(WALL + RED + BOLD + x + " ".repeat(WIDTH - x.length())+ RESET + WALL);
+            System.out.println(BL + WHITE + BOLD + H.repeat(WIDTH) + BR + RESET); // Bottom Border
+
+            System.out.print(BOLD + "Choose Option Using Letters: ");
             String homeChoice = userInput.nextLine().trim().toUpperCase(); //.lowercase
+
+
 
             switch (homeChoice){
                 case "D" -> {
